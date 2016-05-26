546901adde0b9ac11fc5d2a865ccdd7e521d64dc
Yangchen <shyangch@gmail.com>
2016-05-26 18:34:54 -0500

    My first commit

--- labs/lab0/RobotInstructions.java
+++ labs/lab0/RobotInstructions.java
@@ -27,8 +27,8 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
+		robot.forward(80);
+		robot.turnLeft(190);
 		robot.forward(23);
 		robot.turnRight(90);
 		robot.forward(80);
