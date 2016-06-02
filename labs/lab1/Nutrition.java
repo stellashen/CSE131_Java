8f7313d38d45b1bdddc68d76296e548a06b08cb1
Yangchen <shyangch@gmail.com>
2016-06-02 17:43:58 -0500

    revised errors

--- labs/lab1/Nutrition.java
+++ labs/lab1/Nutrition.java
@@ -14,19 +14,19 @@ public class Nutrition {
 		double protein = ap.nextDouble("What's the number of grams of protein in this food?");
 		double statedCals = ap.nextDouble("What's the number of calories stated on this food's label?");
 		
-		double carbsCal = Math.round(40*carbs)/10;
-		double fatCal = Math.round(90*fat)/10;
-		double proteinCal = Math.round(40*protein)/10;
+		double carbsCal = Math.round(10.0*4.0*carbs)/10.0;
+		double fatCal = Math.round(10.0*9.0*fat)/10.0;
+		double proteinCal = Math.round(10.0*4.0*protein)/10.0;
 		double fiberCal = statedCals - carbsCal - fatCal - proteinCal;
-		double fiber = Math.round(100*fiberCal)/400;
+		double fiber = Math.round(100.0*fiberCal/4)/100.0;
 		
-		double carbsPercent = 100*carbsCal/statedCals;
-		double fatPercent = 100*fatCal/statedCals;
-		double proteinPercent = 100*proteinCal/statedCals;
+		double carbsPercent = 100.0*carbsCal/statedCals;
+		double fatPercent = 100.0*fatCal/statedCals;
+		double proteinPercent = 100.0*proteinCal/statedCals;
 		//round to one decimal place
-		double carbsPercentRound = Math.round(10*carbsPercent)/10;
-		double fatPercentRound = Math.round(10*fatPercent)/10;
-		double proteinPercentRound = Math.round(10*proteinPercent)/10;
+		double carbsPercentRound = Math.round(10.0*carbsPercent)/10.0;
+		double fatPercentRound = Math.round(10.0*fatPercent)/10.0;
+		double proteinPercentRound = Math.round(10.0*proteinPercent)/10.0;
 		
 		boolean lowCarb = true;
 		boolean lowFat = true;
@@ -45,10 +45,10 @@ public class Nutrition {
 		System.out.println(name + " has");
 		System.out.println("  " + carbs + " grams of carbohydrates = " + carbsCal + " Calories");
 		System.out.println("  " + fat + " grams of fat = " + fatCal + " Calories");
-		System.out.println("  " + carbs + " grams of protein = " + proteinCal + " Calories");
+		System.out.println("  " + protein + " grams of protein = " + proteinCal + " Calories");
 		System.out.println("  ");
 		System.out.println("This food is said to have " + statedCals + " Calories.");
-		System.out.println("With "+fiberCal+" unavailable Calories, " + "this food has "+fiber+" grams of fiber");
+		System.out.println("With "+fiberCal+" unavailable Calories, " + "this food has " + fiber + " grams of fiber");
 		System.out.println("  ");
 		System.out.println("Approximately");
 		System.out.println("  " +carbsPercentRound+ "% of your food is carbohydrates");
