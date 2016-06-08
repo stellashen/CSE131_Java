c5f40df2459b1844e935e2c6b5bbfd0f058b581b
Yangchen <shyangch@gmail.com>
2016-06-08 00:10:18 -0500

    Add warning messages for problem 1

--- studios/studio2/gambler.java
+++ studios/studio2/gambler.java
@@ -10,55 +10,71 @@ public class gambler {
 		double winChance = ap.nextDouble("What is the probability that you win a gamble? Enter a number between 0 and 1.");
 		double winAmount = ap.nextDouble("If you reach this amount of money, then you won:");
 		int totalPlays = ap.nextInt("What is the number of times you simulate the problem?");
+		boolean warning = false;
 
-		double lossChance = 1-winChance;
-		double ruin = 1.0;
-		int numLoss = 0;
-		
-		//calculate expected ruin rate		
-		if (lossChance != winChance) {
-			double ratio = lossChance/winChance;
-			ruin = (Math.pow(ratio,startAmount)-Math.pow(ratio, winAmount))/(1-Math.pow(ratio, winAmount));
-		}
-		else {
-			ruin = 1 - startAmount / winAmount;
+		if (startAmount >= winAmount){
+			System.out.println("Warning: The amount of money that you start with is greater than the amount of money to win. Please try again and enter different numbers.");
+			System.out.println();
+			warning = true;
 		}
 
-		//simulation totalPlays times
-		for (int i = 1; i <= totalPlays; i++){
-			int numRounds = 0;
-			double money = startAmount;
-			while (money > 0 && money < winAmount){
-				if (Math.random() < winChance){
-					money = money + 1;
+		if (winChance < 0 || winChance > 1) {
+			System.out.println("Warning: The probability of winning has to be within 0 - 1. Please enter a different number.");
+			System.out.println();
+			warning = true;
+		}
+
+		if (warning = false){
+			double lossChance = 1-winChance;
+			double ruin = 1.0;
+			int numLoss = 0;
+
+			//calculate expected ruin rate		
+			if (lossChance != winChance) {
+				double ratio = lossChance/winChance;
+				ruin = (Math.pow(ratio,startAmount)-Math.pow(ratio, winAmount))/(1-Math.pow(ratio, winAmount));
+			}
+			else {
+				ruin = 1 - startAmount / winAmount;
+			}
+
+			//simulation totalPlays times
+			for (int i = 1; i <= totalPlays; i++){
+				int numRounds = 0;
+				double money = startAmount;
+				while (money > 0 && money < winAmount){
+					if (Math.random() < winChance){
+						money = money + 1;
+					}
+					else {
+						money = money - 1;
+					}
+					numRounds = numRounds + 1;
+				}
+
+				if (numRounds == 1) {
+					System.out.println("Simulation " + i + ": " + numRounds + " round  	");
 				}
 				else {
-					money = money - 1;
+					System.out.print("Simulation " + i + ": " + numRounds + " rounds  	");	
 				}
-				numRounds = numRounds + 1;
+
+				if (money <= 0){
+					System.out.println("LOSE");
+					numLoss = numLoss + 1; 
+				}
+				else {
+					System.out.println("WIN");
+				}
 			}
 
-			if (numRounds == 1) {
-				System.out.println("Simulation " + i + ": " + numRounds + " round  	");
-			}
-			else {
-				System.out.print("Simulation " + i + ": " + numRounds + " rounds  	");	
-			}
-
-			if (money <= 0){
-				System.out.println("LOSE");
-				numLoss = numLoss + 1; 
-			}
-			else {
-				System.out.println("WIN");
-			}
+			System.out.println();
+			System.out.print("Losses: " + numLoss);
+			System.out.println("  Simulations: " + totalPlays);
+			System.out.print("Actual Ruin Rate: " + 1.0*numLoss/totalPlays);
+			System.out.println("  Expected Ruin Rate: " + ruin);
 		}
 
-		System.out.println();
-		System.out.print("Losses: " + numLoss);
-		System.out.println("  Simulations: " + totalPlays);
-		System.out.print("Actual Ruin Rate: " + 1.0*numLoss/totalPlays);
-		System.out.println("  Expected Ruin Rate: " + ruin);
 	}
 
 }
