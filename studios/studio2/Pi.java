d44c0d98172155ac6e2118cd6d748da7cac4052a
Yangchen <shyangch@gmail.com>
2016-06-08 22:19:01 -0500

    Completed studio 2

--- studios/studio2/Pi.java
+++ studios/studio2/Pi.java
@@ -1,20 +1,26 @@
 package studio2;
 
+import cse131.ArgsProcessor;
+
 public class Pi {
-	public static void computePi() {
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int N = ap.nextInt("How many times do you want to throw the darts?");
+		int numCircle = 0;
 		double ans = 0.0;
 
-	    // fill in to compute ans = Pi
-		// r = 1, area of unit circle = Pi
-		
-		
+		// fill in to compute ans = Pi
+		// area of 1/4 circle is 1/4*Pi, area of square is 1 
 
-	    System.out.println("Our group shows Pi = " + ans);
-	  }
-	
-	public static void main(String[] args) {
-		computePi();
-
+		for (int i=0;i<N;i++){
+			double x = Math.random();
+			double y = Math.random();
+			double r = Math.sqrt(x*x+y*y);
+			if (r <= 1){
+				numCircle=numCircle+1;
+			}
+		}
+		ans = 4.0*numCircle/N;
+		System.out.println("Our group shows Pi = " + ans);
 	}
-
 }
