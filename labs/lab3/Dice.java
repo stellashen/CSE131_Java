9950ef5371b6fef30cc3bb69088976095ad2df7b
Yangchen <shyangch@gmail.com>
2016-06-16 16:17:49 -0500

    lab3

--- labs/lab3/Dice.java
+++ labs/lab3/Dice.java
@@ -10,6 +10,9 @@ public class Dice {
 		int T = ap.nextInt("How many times will the dice be thrown?");
 		System.out.println("The number of dice: "+D);
 		System.out.println("The number of times that the dice will be thrown: "+T);
+		
+		System.out.println();
+		System.out.println("Iterations:");
 
 		// Q7:how often all of the dice show the same value(e.g. 6 6 6)
 		int dice[]= new int[D];
@@ -32,7 +35,7 @@ public class Dice {
 
 			// Q9. print out the dice values' sum each time the dice are thrown
 			System.out.println();
-			System.out.println("The sum of dice values for iteration " + t + " is " + sumRow);
+			System.out.println("The sum of dice values for iteration " + (t+1) + " is " + sumRow);
 			for (int d=0;d<D;++d){
 				if (dice[d] == sumRow/D){
 					numEqualAvg = numEqualAvg+1;
@@ -48,7 +51,7 @@ public class Dice {
 
 		}
 
-		double percentSameValue = Math.round(100.0*numSameValue/T);
+		int percentSameValue = (int)Math.round(100.0*numSameValue/T);
 		System.out.println();
 		System.out.println("The event that all dices have the same value occurs " + numSameValue + " out of " + T + " times (" 
 				+ percentSameValue + "%).");
@@ -58,8 +61,9 @@ public class Dice {
 		// number of possible sums: 6*D-D+1. so that's the length of array numSum[]
 		// add one line in the iteration above 
 		// and report:
+		System.out.println();
+		System.out.println("The percent of times each value of sum occurs:");
 		for (int i=0;i < (5*D+1);++i){
-			System.out.println("The percent of times each value of sum occurs:");
 			System.out.println("Sum is "+(i+D)+ ": " + Math.round(100.0*numSum[i]/T) + "%");	
 		}
 
