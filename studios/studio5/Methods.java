aa9414c0bc490f6ed6e8d6fbfd6d177cb8895d37
Yangchen <shyangch@gmail.com>
2016-06-30 21:11:11 -0500

    updated studio 5

--- studios/studio5/Methods.java
+++ studios/studio5/Methods.java
@@ -19,8 +19,41 @@ public class Methods {
 	 * @return the product of the factors
 	 */
 	public static int mpy(int x, int y) {
-		return 0;  // FIXME
+		return x*y; 
+	}
+	
+	/**
+	 * 
+	 * @param x one double
+	 * @param y another double
+	 * @param z another double
+	 * @return the average of the three numbers
+	 */
+	public static double avg3(double x, double y, double z) {
+		double avg = (x + y + z)/3.0;
+		return avg; 
 	}
 
+	public static double sumArray(double[] a) {
+		double sumA = 0;
+		for (int i = 0; i < a.length; ++i){
+			sumA = sumA + a[i];
+		}
+		return sumA; 
+	}
+	
+	public static double average(double[] a) {
+		double avgA = sumArray(a)/ a.length;
+		return avgA; 
+	}
+	
+	public static String pig(String s){
+		String pigLatin = s.substring(1)+s.substring(0,1)+"ay";
+		return pigLatin;
+	}
+	
+
+	
+	
 
 }
--- studios/studio5/MethodsTest.java
+++ studios/studio5/MethodsTest.java
@@ -12,6 +12,7 @@ public class MethodsTest {
 		// Try something simple
 		//
 		assertEquals(7, Methods.sum(3, 4));
+		assertEquals(-7, Methods.sum(-3, -4));
 		//
 		// Try something fancier
 		//
@@ -22,8 +23,8 @@ public class MethodsTest {
 	
 	@Test
 	public void thisTestShouldFail() {
-		assertEquals(1, Methods.mpy(0,3));
-		assertEquals(1, Methods.mpy(2, 5));
+		assertEquals(0, Methods.mpy(0,3));
+		assertEquals(10, Methods.mpy(2, 5));
 	}
 	
 	@Test
