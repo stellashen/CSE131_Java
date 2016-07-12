72d78cf0e2e560380147f0144acd98d16da646e5
Yangchen <shyangch@gmail.com>
2016-07-11 21:54:10 -0500

    quiz5

---
+++ exercises/exercises5/quiz5.java
@@ -0,0 +1,15 @@
+package exercises5;
+
+public class quiz5 {
+	
+	public static double circleArea(double r){
+		return Math.PI*r*r;
+	}
+
+	public static void main(String[] args) {
+		double area = circleArea(5.2);
+		System.out.println(area);
+
+	}
+
+}
